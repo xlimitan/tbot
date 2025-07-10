5a9a1e8a3e7c4205e5dfbf1614df19742637a358
xlimitan <xlimitan@yandex.ru>
2025-07-10 14:49:58 +0700

    first commit

---
+++ src/main/java/com/tbot/tbot/TbotApplication.java
@@ -0,0 +1,13 @@
+package com.tbot.tbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbot/tbot/TbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbot.tbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
